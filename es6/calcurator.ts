eb56e9d4ae0a86d0579e9749b78b63e2ee0cd790
jino9492 <jino9492@gmail.com>
2021-06-13 03:42:22 +0900

    ts

--- es6/calcurator.ts
+++ es6/calcurator.ts
@@ -1,17 +1,17 @@
 // 단일 객체 내보내기
-function sum(a, b) {
+function sum(a: number, b: number) {
   return a + b;
 }
 
-function sub(a, b) {
+function sub(a: number, b: number) {
   return a - b;
 }
 
-function multiply(a, b) {
+function multiply(a:number, b: number) {
   return a * b;
 }
 
-function divide(a, b) {
+function divide(a: number, b: number) {
   if (b === 0) throw new Error("0으로 숫자를 나눌 수 없습니다.");
   return a / b;
 }
